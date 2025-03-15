548e351caccfaae20e354a089fde0b4880718017
oycm <1164864987@qq.com>
2025-03-15 13:28:53 +0800

    支持域名校验

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,9 +14,9 @@ export default defineUserConfig({
         "name": "algolia-site-verification", 
         "content": "9C077AD22DBCE0BD",
         // 表示所有http协议增强为https协议
-        'http-equiv': 'Content-Security-Policy',
+        //'http-equiv': 'Content-Security-Policy',
         // 本地启动需要注释，否则本地访问资源切换成https
-        "content": "upgrade-insecure-requests"
+        //"content": "upgrade-insecure-requests"
       },
 
     ]
